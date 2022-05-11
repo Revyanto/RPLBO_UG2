b890e5c1f8caa1d5cc4cd0508587d831a1cd35a8
Revyanto <79372943+Revyanto@users.noreply.github.com>
2022-05-11 19:38:30 +0800

    Delete UG10A_71200610 directory

--- UG10A_71200610/src/test/java/BEBAS/AppTest.java
+++
@@ -1,9 +0,0 @@
-package BEBAS;
-
-public class AppTest
-{
-    public static void main (String[] args) throws PasswordException, UsernameException, EmailException, LoginException {
-        User regis = new User();
-
-    }
-}
