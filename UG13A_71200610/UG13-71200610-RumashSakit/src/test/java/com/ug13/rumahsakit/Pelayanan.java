3da1085311f068c7c85c57c137df91b0afe7ec9b
Revyanto <79372943+Revyanto@users.noreply.github.com>
2022-06-01 20:00:01 +0800

    Delete UG13A_71200610/UG13-71200610-RumashSakit/src directory

--- UG13A_71200610/UG13-71200610-RumashSakit/src/test/java/com/ug13/rumahsakit/Pelayanan.java
+++
@@ -1,9 +0,0 @@
-package com.ug13.rumahsakit;
-
-public class Pelayanan {
-    private String nama;
-
-    public void mengaturJadwalPasien(Pasien pasien,Dokter dokter, Jadwal jadwal){
-
-    }
-}
--- UG13A_71200610/UG13-71200610-RumashSakit/src/test/java/com/ug13/rumahsakit/Suster.java
+++
@@ -1,9 +0,0 @@
-package com.ug13.rumahsakit;
-
-public class Suster {
-    private String nama;
-
-    public void screening(Pasien pasien,Jadwal jadwal){
-
-    }
-}
